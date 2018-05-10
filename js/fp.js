d98ffb92e5544ccbe47fc320d9c379029170afd7
ibkuzr <33265082+ibkuzr@users.noreply.github.com>
2018-05-10 16:00:38 -0400

    Add files via upload

---
+++ js/fp.js
@@ -0,0 +1,7 @@
+function Yes(){
+    document.getElementById("demo").innerHTML="It's time to visit again.";
+
+}
+function No(){
+    document.getElementById("demo2").innerHTML="What are you waiting for? Visit soon.";
+}
